7ac8a78ed41dd3df9549bf4c62e0c6eefdd95cca
aliyenurerdogu <aliyenurerdogu2001@gmail.com>
2025-03-14 16:18:29 +0300

    css + geoloaction

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,5 +1,9 @@
 document.getElementById('getWeatherButton').addEventListener('click', getWeather);
 
+window.onload = () => {
+    getWeather();
+    loadPreviousWeather();
+}
 async function getWeather() {
     const city = document.getElementById('city').value;
     const apiKey = '50a4ee2644953270c63191074ca8f1ef';
@@ -9,6 +13,7 @@ async function getWeather() {
 
     if (city) {
         url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        saveWeather(city);
     } else {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(async (position) => {
@@ -37,21 +42,26 @@ async function getWeather() {
         const response = await fetch(url);
         const data = await response.json();
         updateWeatherData(data);
+
     } catch (error) {
         console.error('Error fetching weather data:', error);
     }
-}
+};
+
 updateWeatherData = (data) => {
     if (data.cod === 200) {
         const iconCode = data.weather[0].icon;
         const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
+        const humidityIcon = `images/humidity.png`;
+        const windIcon = `images/windy.png`;
 
         document.getElementById('weatherIcon').textContent = iconUrl;
         document.getElementById('cityName').textContent = data.name;
-        document.getElementById('temperature').textContent = `Temperature: ${data.main.temp}°C`;
-        document.getElementById('humidity').textContent = `Humidity: ${data.main.humidity}%`;
-        document.getElementById('wind').textContent = `Wind speed: ${data.wind.speed}m/s`;
-        document.getElementById('weatherIcon').src = iconUrl;
+        document.getElementById('temperature').textContent = `${data.main.temp}°C`;
+        document.getElementById('humidity').textContent = `${data.main.humidity}%`;
+        document.getElementById('wind').textContent = `${data.wind.speed}m/s`;
+        document.getElementById('humidityIcon').src = humidityIcon;
+        document.getElementById('windIcon').src = windIcon;
         if (data.coord) {
             document.getElementById('weatherDetails').textContent =
                 `Latitude: ${data.coord.lat}, Longitude: ${data.coord.lon}`;
